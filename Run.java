2fbc50f63e90f2302f5214489fa1e147000202dc
astik1991 <suhane.astik@gmail.com>
2021-05-06 10:20:33 +0530

    did some changes

--- Run.java
+++ Run.java
@@ -1,7 +1,7 @@
 class Run{
 public static void main(String[] a){
 for(int i=0;i<10;i++){
-	System.out.println("Jenkins Job Done ...... " + i);
+	System.out.println("Success Full Done  ...... " + i);
 }
 
 }
